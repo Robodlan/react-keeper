047040892a507a424b8d7cebde6f4ea6a2324df8
Robodlan <reinaldo.robles@icloud.com>
2024-02-28 18:34:24 -0400

    hooks

--- vite-keeper/src/components/App.jsx
+++ vite-keeper/src/components/App.jsx
@@ -1,35 +1,30 @@
 import React, { useState } from "react";
 
 function App() {
-  let time = new Date().toLocaleTimeString();
-  const [timer, setTimer] = useState(new Date().toLocaleTimeString());
+  const [name, setName] = useState({
+    fName: firstName,
+    lName: lastName
+  });
+  const [last, setLast] = useState("");
 
-  function show() {
-    setTimer(timer);
+  function handleChange(e) {
+   setName(e.target.value)
   }
+
+  function handleLast(e) {
+   setLast(e.target.value)
+  };
 
   return (
     <div className="container">
-      <h1>{timer}</h1>
-      <button onClick={show}></button>
+      <h1>Hello {name}{last}</h1>
+      <form>
+        <input onChange={handleChange} name="fName" placeholder="First Name"value={name}/>
+        <input onChange={handleLast} name="lName" placeholder="Last Name" value={last}/>
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
 
 export default App;
-
-// /Challenge:
-//1. Given that you can get the current time using:
-
-//Show the latest time in the <h1> when the Get Time button
-//is pressed.
-
-//2. Given that you can get code to be called every second
-//using the setInterval method.
-//Can you get the time in your <h1> to update every second?
-
-//e.g. uncomment the code below to see Hey printed every second.
-// function sayHi() {
-//   console.log("Hey");
-// }
-// setInterval(sayHi, 1000);
