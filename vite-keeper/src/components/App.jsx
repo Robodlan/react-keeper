b798a354fce96a2b8523512bda1ed61f4262b66a
Robodlan <reinaldo.robles@icloud.com>
2024-02-28 21:32:21 -0400

    destructuring

--- vite-keeper/src/components/App.jsx
+++ vite-keeper/src/components/App.jsx
@@ -1,34 +1,45 @@
 import React, { useState } from "react";
 
 function App() {
-  const [fullName, setFullName] = useState({
+  const [contact, setContact] = useState({
     fName: "",
-    lName: ""
-  })
+    lName: "",
+    email: ""
+  });
 
-function handleChange(event) {
-  const {name, value} = event.target;
-
-  setFullName(prev => {
-    if (name === "fName") {
+  function handleChange(event) {
+   const {name, value} = event.target;
+   setContact(prev => {
+    if (name === "fName"){
       return {
         fName: value,
         lName: prev.lName
-      };
+        };
     } else if (name === "lName") {
       return {
         fName: prev.fName,
         lName: value
       }
+    } else if (name === "email") {
+      return {
+        fName: prev.fName,
+        lName: prev.lName,
+        email: value
+      }
     }
-  })
-}
-return (
+   })
+  }
+
+  return (
     <div className="container">
-      <h1>Hello {fullName.fName} {fullName.lName} </h1>
+      <h1>
+        Hello {contact.fName} {contact.lName}
+      </h1>
+      <p>{contact.email}</p>
       <form>
-        <input onChange={handleChange} name="fName" placeholder="First Name" value={fullName.fName}/>
-        <input onChange={handleChange} name="lName" placeholder="Last Name" value={fullName.lName}/>
+        <input onChange={handleChange} name="fName" placeholder="First Name" value={contact.fName}/>
+        <input onChange={handleChange} name="lName" placeholder="Last Name" value={contact.lName}/>
+        <input onChange={handleChange} name="email" placeholder="Email" value={contact.email}/>
         <button>Submit</button>
       </form>
     </div>
@@ -36,3 +47,14 @@ return (
 }
 
 export default App;
+
+
+//CHALLENGE: Make the code in App.jsx work.
+//The final app should have a single contact
+//with fName, lName and email.
+
+//HINT: You'll need to apply the following things you learnt:
+//1. Using JS Objects with state.
+//2. Making use of previous state when changing state.
+//3. Working with forms in React.
+//4. Handing events
