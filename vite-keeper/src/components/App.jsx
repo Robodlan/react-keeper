d2bb439fbf33060214611ba3a29b59a32ce3524c
Robodlan <reinaldo.robles@icloud.com>
2024-03-03 19:32:21 -0400

    input added

--- vite-keeper/src/components/App.jsx
+++ vite-keeper/src/components/App.jsx
@@ -1,6 +1,4 @@
 import React, { useState } from "react";
-import ToDoItem from "./ToDoItem";
-import InputArea from "./InputArea";
 
 function App() {
   const [inputText, setInputText] = useState("");
@@ -18,36 +16,21 @@ function App() {
     setInputText("");
   }
 
-  function deleteItem(id) {
-    setItems(prevItems => {
-      return prevItems.filter((item, index) => {
-        return index !== id;
-      });
-    });
-  }
-
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
-      {inputText.map(value => {
-        <InputArea 
-          text={value}
-          onClick={handleChange}
-          onCheck={addItem}
-        />
-        
-        })}
+      <div className="form">
+        <input onChange={handleChange} type="text" value={inputText} />
+        <button onClick={addItem}>
+          <span>Add</span>
+        </button>
+      </div>
       <div>
         <ul>
-          {items.map((todoItem, index) => (
-            <ToDoItem
-              key={index}
-              id={index}
-              text={todoItem}
-              onChecked={deleteItem}
-            />
+          {items.map(todoItem => (
+            <li>{todoItem}</li>
           ))}
         </ul>
       </div>
@@ -56,17 +39,3 @@ function App() {
 }
 
 export default App;
-
-
-//CHALLENGE: I have extracted the Input Area, including the <input> and
-//<button> elements into a seperate Component called InputArea.
-//Your job is to make the app work as it did before but this time with the
-//InputArea as a seperate Component.
-
-// DO NOT: Modify the ToDoItem.jsx
-// DO NOT: Move the input/button elements back into the App.jsx
-
-//Hint 1: You will need to think about how to manage the state of the input element
-//in InputArea.jsx.
-//Hint 2: You will need to think about how to pass the input value back into
-//the addItem() function in App.jsx.
