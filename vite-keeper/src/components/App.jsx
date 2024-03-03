34f9acd52af2fd9d988947b84e3a8ee767f093f4
Robodlan <reinaldo.robles@icloud.com>
2024-03-02 21:00:37 -0400

    functional proggraming

--- vite-keeper/src/components/App.jsx
+++ vite-keeper/src/components/App.jsx
@@ -17,6 +17,14 @@ function App() {
     setInputText("");
   }
 
+  function deleteItem(id) {
+    setItems(prev => {
+      return prev.filter((item, index) => {
+        return index !== id;
+      })
+    })
+  }
+
   return (
     <div className="container">
       <div className="heading">
@@ -30,8 +38,8 @@ function App() {
       </div>
       <div>
         <ul>
-          {items.map(todoItem => (
-          <ToDoItem item={todoItem} />
+          {items.map((todoItem, index) => (
+          <ToDoItem key={index} id={index} item={todoItem}  onCheck={deleteItem} />
           ))}
         </ul>
       </div>
--- vite-keeper/src/components/ToDoItem.jsx
+++ vite-keeper/src/components/ToDoItem.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
 function ToDoItem(props) {
-    function handleClick() {
-
-    }
-    return (
-        <div onClick={handleClick} >
-            <li style={{textDecoration: "line-through"}}>{props.item}</li>
+  return (
+        <div onClick={()=> {
+         props.onCheck(props.id)
+         console.log(props.id)
+        }} >
+            <li>{props.item}</li>
         </div>
     )
 }
