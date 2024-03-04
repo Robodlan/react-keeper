7fad7d4c6d04cf829b9dbb26bf24cd18bb9935f2
Robodlan <Robodlan@users.noreply.github.com>
2024-03-04 07:21:37 -0400

    underline

--- vite-keeper/src/components/ToDoItem.jsx
+++ vite-keeper/src/components/ToDoItem.jsx
@@ -1,17 +1,18 @@
 import React, { useState } from "react";
 
 function ToDoItem(props) {
-    const [state, setState] = useState(false)
+  const [state, setState] = useState(false);
 
-    function handleClick() {
-        setState(true)
-    };
+  function handleClick() {
+    setState(true);
+  }
   return (
-        <div onClick={handleClick} >
-            <li style={{textDecoration: state ? "line-through" : "none"}} >{props.item}</li>
-        </div>
-    )
+    <div onClick={handleClick}>
+      <li style={{ textDecoration: state ? "underline" : "none" }}>
+        {props.item}
+      </li>
+    </div>
+  );
 }
 
 export default ToDoItem;
-
