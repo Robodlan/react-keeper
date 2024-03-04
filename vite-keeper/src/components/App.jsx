02bc987579eb571bd2cc16d2b0f6be3a9b507449
Robodlan <Robodlan@users.noreply.github.com>
2024-03-04 07:22:35 -0400

    delete todo

--- vite-keeper/src/components/App.jsx
+++ vite-keeper/src/components/App.jsx
@@ -1,31 +1,30 @@
 import React, { useState } from "react";
-import ToDoItem from "./ToDoItem"
 
 function App() {
   const [inputText, setInputText] = useState("");
   const [items, setItems] = useState([]);
 
   function handleChange(event) {
-   const {name, value} = event.target;
-   setContact(prev => {
-    if (name === "fName"){
-      return {
-        fName: value,
-        lName: prev.lName
+    const { name, value } = event.target;
+    setContact((prev) => {
+      if (name === "fName") {
+        return {
+          fName: value,
+          lName: prev.lName,
         };
-    } else if (name === "lName") {
-      return {
-        fName: prev.fName,
-        lName: value
+      } else if (name === "lName") {
+        return {
+          fName: prev.fName,
+          lName: value,
+        };
+      } else if (name === "email") {
+        return {
+          fName: prev.fName,
+          lName: prev.lName,
+          email: value,
+        };
       }
-    } else if (name === "email") {
-      return {
-        fName: prev.fName,
-        lName: prev.lName,
-        email: value
-      }
-    }
-   })
+    });
   }
 
   return (
@@ -35,9 +34,24 @@ function App() {
       </h1>
       <p>{contact.email}</p>
       <form>
-        <input onChange={handleChange} name="fName" placeholder="First Name" value={contact.fName}/>
-        <input onChange={handleChange} name="lName" placeholder="Last Name" value={contact.lName}/>
-        <input onChange={handleChange} name="email" placeholder="Email" value={contact.email}/>
+        <input
+          onChange={handleChange}
+          name="fName"
+          placeholder="First Name"
+          value={contact.fName}
+        />
+        <input
+          onChange={handleChange}
+          name="lName"
+          placeholder="Last Name"
+          value={contact.lName}
+        />
+        <input
+          onChange={handleChange}
+          name="email"
+          placeholder="Email"
+          value={contact.email}
+        />
         <button>Submit</button>
       </form>
     </div>
