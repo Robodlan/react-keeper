f08cffe1a5258cf511671fc2babf99f15509f376
Robodlan <reinaldo.robles@icloud.com>
2024-03-06 21:31:39 -0400

    keeper add Note

--- vite-keeper/src/components/CreateArea.jsx
+++ vite-keeper/src/components/CreateArea.jsx
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CreateArea() {
-  return (
-    <div>
-      <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
-      </form>
-    </div>
-  );
+function CreateArea(props) {
+    const [note, setNote] = useState({
+        title: "",
+        content: ""
+    })
+
+    function handleChange(e) {
+        const { name, value } = e.target;
+        setNote(prev => {
+            return {
+                ...prev,
+                [name]: value,
+            }
+        });
+    };
+
+    function submitNote(e) {
+        props.onAdd(note);
+        e.preventDefault();
+    };
+
+    return (
+        <div>
+            <form>
+                <input onChange={handleChange} name="title" placeholder="Title" value={note.title} />
+                <textarea onChange={handleChange} name="content" placeholder="Take a note..." rows="3" value={note.content} />
+                <button onClick={submitNote} >Add</button>
+            </form>
+        </div>
+    );
 }
 
 export default CreateArea;
